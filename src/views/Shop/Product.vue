3637df326b998c5e00c903c9e0587b9a3645147e
Katie <kitkatkadams@gmail.com>
2020-05-03 21:05:54 +0100

    Conditional formatting for adult shop items

--- src/views/Shop/Product.vue
+++ src/views/Shop/Product.vue
@@ -5,7 +5,8 @@
         <ToggleButton v-model="adultEnabled"/>
         <GetData url="https://studentweb.staffs.ac.uk/a010743g/mcintireFruits/wp-json/wp/v2/shop_items?_embed=wp:term,wp:featuredmedia&item_type=14"/>
         <div v-if="adultEnabled === true">
-            
+            <h2>Alcohol</h2>
+            <GetData url="https://studentweb.staffs.ac.uk/a010743g/mcintireFruits/wp-json/wp/v2/shop_items?_embed=wp:term,wp:featuredmedia&item_type=15"/>
         </div>
     </div>
 </template>
